895fc538ef4b3f1a66f549ddc70390ecb0907e50
Martin Mollay <martin@ssi.at>
2024-12-08 14:13:21 +0100

    Update newsletter version to 1.2.2

--- modules/newsletter/functions.php
+++ modules/newsletter/functions.php
@@ -180,4 +180,3 @@ function prepareHtmlForEmail($content)
 
     return $content;
 }
-
--- modules/newsletter/version.php
+++ modules/newsletter/version.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'version' => '1.2.1',
+    'version' => '1.2.2',
     'changelog' => [
 
         '1.1.3' => [
