5cd4d2aa8772515127e83ea8751bb0f5c977aa47
Martin Mollay <martin@ssi.at>
2024-12-09 00:22:10 +0100

    Update editor_config.php with new min and max heights

--- modules/newsletter/components/editor_config.php
+++ modules/newsletter/components/editor_config.php
@@ -3,8 +3,8 @@ function getEditorConfig($userId, $update_id)
 {
 
     return [
-        'minHeight' => 400,
-        //'maxHeight' => 800,
+        'minHeight' => 600,
+        'maxHeight' => 1000,
         'placeholder' => 'Geben Sie hier Ihre Nachricht ein...',
         'image' => [
             'upload' => [
